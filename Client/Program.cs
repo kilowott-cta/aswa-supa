a6f5b8af55951345cd8fa16e8b62968601d33831
kilowott cta <gilroy.menezes@kilowott.com>
2026-01-02 15:34:44 +0530

    Read from supabase

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,4 +8,16 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+var configuration = builder.Configuration;
+var supabaseUrl = configuration["Supabase:Url"] ?? configuration["SUPABASE_URL"] ?? string.Empty;
+var supabaseKey = configuration["Supabase:Key"] ?? configuration["SUPABASE_KEY"] ?? string.Empty;
+
+var options = new Supabase.SupabaseOptions
+{
+    AutoConnectRealtime = true,
+};
+
+var supabase = new Supabase.Client(supabaseUrl, supabaseKey, options);
+builder.Services.AddSingleton(supabase);
+
 await builder.Build().RunAsync();

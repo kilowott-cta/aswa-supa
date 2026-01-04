2bc7266165628bbe3ab4ba0e4f2ee6f943eb477a
kilowott cta <gilroy.menezes@kilowott.com>
2026-01-04 15:46:20 +0530

    Fixed broken function

--- Api/Heath.cs
+++ Api/Heath.cs
@@ -7,17 +7,17 @@ using Microsoft.Extensions.Logging;
 
 namespace Function.Basic;
 
-public class HealthCheck
+public class Health
 {
-    private readonly ILogger<HealthCheck> _logger;
+    private readonly ILogger<Health> _logger;
 
-    public HealthCheck(ILogger<HealthCheck> logger)
+    public Health(ILogger<Health> logger)
     {
         _logger = logger;
     }
 
-    [Function("healthcheck")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    [Function("pingpong")]
+    public IActionResult PingPong([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
         ClaimsPrincipal user = req.HttpContext.User;

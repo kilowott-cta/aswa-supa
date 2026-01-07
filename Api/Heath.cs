2978fe0af80b513ccbcc1a1508df7505095cef0b
kilowott cta <gilroy.menezes@kilowott.com>
2026-01-07 18:37:08 +0530

    Error message on dash

--- Api/Heath.cs
+++ Api/Heath.cs
@@ -49,26 +49,6 @@ public class Health
 
         var projects = results.Models.Select(p => p.ToDto());
 
-        // var projects = results.Models.Select(p => new DomainBasic.Models.Dto.Project
-        // {
-        //     ProjectId = p.ProjectId,
-        //     ProjectName = p.ProjectName,
-        //     Stage = p.Stage,
-        //     Status = p.Status,
-        //     ClientName = p.ClientName,
-        //     AccountManager = p.AccountManager,
-        //     Designers= p.Designers,
-        //     Architects = p.Architects,
-        //     Analysts = p.Analysts,
-        //     Tags = p.Tags,
-        //     SoldHours = p.SoldHours,
-        //     BallparkHours = p.BallparkHours,
-        //     Owner = p.Owner,
-        //     PresalesPriority = p.PresalesPriority,
-        //     Skillsets = p.Skillsets,
-        //     UpdatedAt = p.UpdatedAt,
-        //     CreatedAt = p.CreatedAt
-        // });
         return new OkObjectResult(projects);
     }
 }

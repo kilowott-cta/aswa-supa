c183d7d054f91a749e7c7cdc26b2f6c4023d8f47
kilowott cta <gilroy.menezes@kilowott.com>
2026-01-11 11:04:23 +0530

    Post to functions

--- Api/Blazor.cs
+++ Api/Blazor.cs
@@ -1,4 +1,5 @@
 using System.Security.Claims;
+using System.Text.Json;
 using DomainBasic.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,6 +14,10 @@ public class Blazor
     private readonly ILogger<Blazor> _logger;
     private readonly IConfiguration _configuration;
     private readonly Supabase.Client _supabaseClient;
+    private readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
+    {
+        PropertyNameCaseInsensitive= true
+    };
 
     private async Task<bool> IsAuthorized(HttpRequest req)
     {
@@ -40,7 +45,7 @@ public class Blazor
         return new OkObjectResult($"Welcome {user?.Identity?.Name} to Functions with {url}.");
     }
 
-    [Function("projects")]
+    [Function("get-projects")]
     public async Task<IActionResult> GetProjects([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
     {
         try
@@ -59,6 +64,33 @@ public class Blazor
         {
             _logger.LogError(ex.Message);
             return new BadRequestResult();
+        }
+    }
+
+    [Function("post-projects")]
+    public async Task<IActionResult> PostProjects([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req)
+    {
+        try
+        {
+            using var reader = new StreamReader(req.Body);
+            var requestBody= await reader.ReadToEndAsync();
+            var items = JsonSerializer.Deserialize<List<DomainBasic.Models.Dto.Project>>(
+                requestBody, _jsonSerializerOptions
+            );
+
+            var token = req.Headers["x-Authorization"].ToString().Replace("Bearer ", "");
+            var session = await _supabaseClient.Auth.SetSession(token, Guid.NewGuid().ToString());
+            
+            var projects = items?.Select(i => i.ToDboFromDto()).ToList();
+            if (projects is null) return new OkObjectResult(projects?.Count);
+            var results = await _supabaseClient
+                .From<DomainBasic.Models.Dbo.Project>().Upsert(projects);
+
+            return new OkObjectResult(projects?.Count);
+        }
+        catch(Exception ex)
+        {
+            return new BadRequestObjectResult(ex.Message);
         }
     }
 
